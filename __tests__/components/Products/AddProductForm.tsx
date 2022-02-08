6c659e123ef285a059a991c573a17a1056f4b3d2
Luis Gonzalez <luis.gonzalez@airbnb.com>
2022-02-07 16:53:32 -0800

    Remnove log of dom on test

--- __tests__/components/Products/AddProductForm.tsx
+++ __tests__/components/Products/AddProductForm.tsx
@@ -1,4 +1,4 @@
-import { screen, prettyDOM } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import AddProductForm from '@components/Products/AddProductForm';
 import renderWithGraphql from '../../../lib/utils/renderWithGraphql';
 
@@ -6,10 +6,9 @@ jest.mock('next/dist/client/router', () => require('next-router-mock'));
 
 describe('Add Product', () => {
   it('should render the Add Product Form', async () => {
-    const { container } = await renderWithGraphql(<AddProductForm />, {
+    await renderWithGraphql(<AddProductForm />, {
       mocks: {},
     });
-    prettyDOM(container);
     expect(screen.getByText('Product Name')).not.toBeNull();
   });
 });
