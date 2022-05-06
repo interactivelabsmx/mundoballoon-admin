0bfd1df40fc8d5dca81de9f97d1925c5e104e0ce
Luis Gonzalez <luis.gonzalez@airbnb.com>
2022-05-05 17:39:23 -0700

    Almost there back with test

--- jest.config.ts
+++ jest.config.ts
@@ -5,18 +5,8 @@ const createJestConfig = nextJest({ dir: './' });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@components/(.*)$': '<rootDir>/components/$1',
-    '^@containers/(.*)$': '<rootDir>/containers/$1',
-    '^@graphql/(.*)$': '<rootDir>/graphql/$1',
-    '^@hooks/(.*)$': '<rootDir>/hooks/$1',
-    '^@layouts/(.*)$': '<rootDir>/layouts/$1',
-    '^@lib/(.*)$': '<rootDir>/lib/$1',
-    '^@pages/(.*)$': '<rootDir>/pages/$1',
-    '^firebase-admin/(.*)$': '<rootDir>/node_modules/firebase-admin/lib/$1',
-  },
   testEnvironment: 'jest-environment-jsdom',
   collectCoverage: true,
   collectCoverageFrom: [
@@ -33,9 +23,6 @@ const customJestConfig = {
       lines: 65,
       statements: 65,
     },
-  },
-  transform: {
-    '\\.(gql|graphql)$': 'jest-transform-graphql',
   },
 };
 
